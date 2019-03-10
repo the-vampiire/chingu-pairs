294870b181d09dd4cb554d0632138ffe1bf8ca9f
The-Vampiire <25523682+the-vampiire@users.noreply.github.com>
2019-03-10 15:23:16 -0400

    Github Auth handlers: login and success handlers

---
+++ server/controllers/auth-controller/github-auth-controller/github-auth.handlers.js
@@ -0,0 +1,36 @@
+const { buildGithubLoginLink } = require('./github-auth.utils');
+const {
+  generateState,
+  setFortifiedCookie,
+  buildAuthRedirect,
+} = require('../auth.utils');
+
+const loginViewHandler = (req, res) => {
+  const { env } = req.context;
+
+  const state = generateState();
+  const loginLink = buildGithubLoginLink(state, env);
+
+  setFortifiedCookie(res, 'state', state, '/auth/github/success');
+
+  return res.render('auth', {
+    loginLink,
+    title: 'Github Auth',
+    imagePath: '/images/octo-walk.gif',
+    buttonText: 'Login with Github',
+  });
+};
+
+const successHandler = (req, res) => {
+  const { env, authToken, authPayload } = req.context;
+
+  const redirectLocation = buildAuthRedirect(authPayload, env);
+
+  setFortifiedCookie(res, 'token', authToken);
+  return res.redirect(redirectLocation);
+};
+
+module.exports = {
+  loginViewHandler,
+  successHandler,
+};
