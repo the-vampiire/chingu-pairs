486977e93c02eb748863d28ddd53518801a5448d
The-Vampiire <25523682+the-vampiire@users.noreply.github.com>
2019-03-10 15:22:11 -0400

    Github Auth utils: buildLoginLink, getEmail, signToken

---
+++ server/controllers/auth-controller/github-auth-controller/github-auth.utils.js
@@ -0,0 +1,72 @@
+const axios = require('axios');
+
+/**
+ * Gets the GitHub User's primary email address
+ * - calls the /user/emails GitHub API endpoint
+ * @param {string} access_token GitHub User access token
+ * @returns {string} primary email address on success
+ * @returns {null} null on access failure
+ * @returns {null} null if no primary email address is listed
+ */
+const getGithubPrimaryEmail = async (access_token) => {
+  let response;
+  try {
+    response = await axios.get('https://api.github.com/user/emails', {
+      headers: { authorization: `Bearer ${access_token}`},
+    });
+  } catch(error) {
+    console.error(error);
+    return null;
+  }
+
+  const primaryEmailData = response.data.find(emailData => emailData.primary);
+  return primaryEmailData ? primaryEmailData.email : null;
+};
+
+/**
+ * Builds a GitHub OAuth login URL used to initiate authorization
+ * @param {string} state random state parameter
+ * @param {object} env environment variables
+ * @param {string} env.DOMAIN local server domain 
+ * @param {string} env.GITHUB_CLIENT_ID client ID for OAuth flow
+ * @throws {Error} if env vars are undefined
+ * @returns {string} a fully formed GitHub OAuth login URL
+ */
+const buildGithubLoginLink = (state, env) => {
+  const { DOMAIN, GITHUB_CLIENT_ID } = env;
+
+  if (!DOMAIN || !GITHUB_CLIENT_ID) {
+    throw new Error('Requires DOMAIN and GITHUB_CLIENT_ID env vars');
+  }
+
+  let loginLink = 'https://github.com/login/oauth/authorize';
+  loginLink += `?client_id=${GITHUB_CLIENT_ID}`;
+  loginLink += `&redirect_uri=${DOMAIN}/auth/github/success`;
+  loginLink += `&state=${state}`;
+
+  return loginLink;
+};
+
+/**
+ * Signs a Chingu Pairs authentication token
+ * - { sub: <user ID> }
+ * @param {User} user authenticated user
+ * @param {object} env environment variables
+ * @returns {string} signed JWT
+ */
+const signAuthToken = (user, env) => {
+  const { id } = user;
+
+  const tokenOptions = {
+    issuer: env.DOMAIN,
+    expiresIn: env.AUTH_TOKEN_EXP,
+  };
+
+  return jwt.sign({ id }, env.AUTH_TOKEN_SECRET, tokenOptions);
+};
+
+module.exports = {
+  signAuthToken,
+  buildGithubLoginLink,
+  getGithubPrimaryEmail,
+}
