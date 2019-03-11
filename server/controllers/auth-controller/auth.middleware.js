b60c8abc673bf9d672023690524040f356e2ffed
The-Vampiire <25523682+the-vampiire@users.noreply.github.com>
2019-03-11 01:20:26 -0400

    Auth MW: extractAuthToken, attachAuthCookie, TODO: tests

--- server/controllers/auth-controller/auth.middleware.js
+++ server/controllers/auth-controller/auth.middleware.js
@@ -17,6 +17,41 @@ const verifyStates = (req, res, next) => {
   next();
 };
 
+// TODO: docs and tests
+const extractAuthToken = (req, res, next) => {
+  const {
+    env,
+    signedCookies: { token },
+  } = req;
+
+  const authToken = verifyAuthToken(token);
+  if (!authToken) {
+    return res.sendStatus(401);
+  }
+  
+  req.context.authToken = authToken;
+  next();
+};
+
+// TODO: tests
+/**
+ * Signs an auth token and attaches it in a fortified cookie
+ * - JWT shape: { sub, iss, exp, registrations }
+ * - cookie shape: token=JWT
+ * @param {object} req.context.env environment variables
+ * @param {object} req.context.authPayload { sub, registrations }
+ */
+const attachAuthCookie = (req, _, next) => {
+  const { env, authPayload } = req.context;
+
+  const authToken = signAuthToken(authPayload, env);
+
+  setFortifiedCookie(res, 'token', authToken);
+  next();
+};
+
 module.exports = {
   verifyStates,
+  extractAuthToken,
+  attachAuthCookie,
 };
