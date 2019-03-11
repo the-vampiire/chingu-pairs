c4ad2e02ee9f99fcc3a10fdedf6fb0f77987f04c
The-Vampiire <25523682+the-vampiire@users.noreply.github.com>
2019-03-11 01:22:01 -0400

    Github Auth MW: flagStatus use new User static

--- server/controllers/auth-controller/github-auth-controller/github-auth.middleware.js
+++ server/controllers/auth-controller/github-auth-controller/github-auth.middleware.js
@@ -1,6 +1,4 @@
 const axios = require('axios');
-const jwt = require('jsonwebtoken');
-
 const { getGithubPrimaryEmail } = require('./github-auth.utils');
 
 /**
@@ -70,68 +68,26 @@ const getGithubUserData = async (req, res, next) => {
 };
 
 /**
- * Builds User auth token payload with User data an registration statuses
+ * Builds User auth token payload with User data and registration statuses
+ * - uses Github data to find or create a User
  * - injects req.context.authPayload and calls next()
- * - authPayload: { registrations: { discord, github, service }, sub: <User ID> }
+ *  - authPayload: { sub: <User ID>, registrations: { discord, service } }
  * @param {object} req.context.models DB models
  * @param {object} req.context.githubUserData GitHub User data
  */
 const flagRegistrationStatus = async (req, _, next) => {
   const { models, githubUserData } = req.context;
 
-  // every time user logs in
-  const authPayload = {
-    sub: null, // sub holds User ID
-    registrations: { discord: false, github: false, service: false },
-  };
-
-  let githubData;
-  try {
-    [githubData] = await models.GithubData.findOrCreate({
-      attributes: [], // no github data needed
-      where: { ...githubUserData }, // idea: what if primary email or username on GitHub have changed?
-      include: [
-        { required: false, model: 'User', attributes: ['id', 'discord_id'] },
-      ],
-    });
-
-    // if await succeeds mark github registration complete
-    registrationStatus.github = true;
-  } catch (error) {
-    console.error(error);
+  const user = await models.User.githubGetOrCreate(githubUserData);
+  if (!user) {
+    // error during saving new user
     return res.sendStatus(500);
   }
 
-  if (githubData.User) {
-    // User entry has been made, mark service registration complete
-    registrationStatus.service = true;
-    // set sub (JWT subject) ID to the User ID
-    registrationStatus.sub = githubData.User.id;
-
-    if (githubData.User.discord_id) {
-      // if User.discord_id is not null mark discord registration complete
-      registrationStatus.discord = true;
-    }
-  }
-
-  req.context.authPayload = authPayload;
-  next();
-};
-
-// TODO: docs and tests
-const signAuthToken = (req, _, next) => {
-  const { env, authPayload } = req.context;
-
-  const tokenOptions = {
-    issuer: env.DOMAIN,
-    expiresIn: env.AUTH_TOKEN_EXP,
+  req.context.authPayload = {
+    sub: user.id,
+    registrations: user.registrationStatus,
   };
-
-  req.context.authToken = jwt.sign(
-    authPayload,
-    env.AUTH_TOKEN_SECRET,
-    tokenOptions
-  );
   next();
 };
 
@@ -139,5 +95,4 @@ module.exports = {
   getGithubAccessToken,
   getGithubUserData,
   flagRegistrationStatus,
-  signAuthToken,
 };
