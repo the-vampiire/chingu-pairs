938b9ba874a75a4fc0fedaaf5634011bd86b74c9
The-Vampiire <25523682+the-vampiire@users.noreply.github.com>
2019-03-12 02:05:56 -0400

    App MW: add db enums to context

--- server/app-middleware/index.js
+++ server/app-middleware/index.js
@@ -2,25 +2,23 @@
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+const enums = require('../../db/enums');
 const models = require('../../db/models');
 
-
-module.exports = (env) => {
-  const injectContext = (context) => (req, _, next) => {
-    req.context = context;
-    next();
-  }
-
-  return {
-    customMiddleware: {
-      injectContext,
-    },
-
-    appMiddleware: [
-      bodyParser.json(),
-      bodyParser.urlencoded({ extended: false }),
-      cookieParser(env.COOKIE_SECRET),
-      injectContext({ env, models }),
-    ],
-  }
+const injectContext = (context) => (req, _, next) => {
+  req.context = context;
+  next();
 }
+
+module.exports = (env) => ({
+  customMiddleware: {
+    injectContext,
+  },
+
+  appMiddleware: [
+    bodyParser.json(),
+    bodyParser.urlencoded({ extended: false }),
+    cookieParser(env.COOKIE_SECRET),
+    injectContext({ env, models, enums }),
+  ],
+});
