50c83235ee4708de5f9b89561dbb9772edee242d
The-Vampiire <25523682+the-vampiire@users.noreply.github.com>
2019-03-12 02:04:48 -0400

    DB: User getters rank -> skill_level to match Model

--- db/models/user/methods/user.getters.js
+++ db/models/user/methods/user.getters.js
@@ -1,11 +1,11 @@
 /**
  * Access the User's registration status
  * - checks if discord and service registrations are complete
+ * - service fields: skill_level, timezone, availability
  * @returns {{ discord: boolean, service: boolean }} 
  */
 function registrationStatus() {
-  const serviceFields = ['rank', 'timezone', 'availability'];
-
+  const serviceFields = ['skill_level', 'timezone', 'availability'];
   return {
     discord: this.discord_id !== null,
     service: serviceFields.every(field => this[field] !== null),
